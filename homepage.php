2d01109b865325ecaf285128c07c27215f1c14f7
Jdelvillar103 <55254086+Jdelvillar103@users.noreply.github.com>
2020-04-15 21:00:42 -0500

    Homepage display adjusments

--- homepage.php
+++ homepage.php
@@ -302,11 +302,12 @@
                      while($row = mysqli_fetch_array($result))  
                      {  
                 ?>
-        <div class="col-md-4">
+        <div class="col-md-4 col-12" style="padding-bottom:35px" align="center">
             <form method="post" action="Test_Cart/Cart.php?action=add&id=<?php echo $row["ID_Product"]; ?>">
-                <div style="border:1px solid #333; background-color:#f1f1f1; border-radius:5px; padding:16px;"
+                <div  class="card" style="border:5px solid purple; background-color:white; border-radius:5px; padding-top:20px; width:47rem; height:65rem;" 
                     align="center">
-                    <img src="<?php echo './assests/'.$row["ImageName"].'.jpg'; ?>" class="img-responsive" /><br />
+                    <img src="<?php echo './assests/'.$row["ImageName"].'.jpg'; ?>" class="img-responsive" style="max-height:340px;"/><br/>
+                    <div class="card-body" style="background-color:lightgray;">
                     <h4 class="text-info"><?php echo $row["PrName"]; ?></h4>
                     <h4 class="text"><?php echo $row["PrDesc"]; ?></h4>
                     <h4 class="text-danger">$ <?php echo $row["Price"]; ?></h4>
@@ -315,6 +316,7 @@
                     <input type="hidden" name="hidden_price" value="<?php echo $row["Price"]; ?>" />
                     <input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-success"
                         value="Add to Cart" />
+                    </div>
                 </div>
             </form>
         </div>
@@ -322,34 +324,7 @@
                      }  
                 }  
                 ?>
-        <div style="clear:both"></div>
-        <?php   
-                          if(!empty($_SESSION["shopping_cart"]))  
-                          {  
-                               $total = 0;  
-                               foreach($_SESSION["shopping_cart"] as $keys => $values)  
-                               {  
-                          ?>
-                <tr>
-                    <td><?php echo $values["item_name"]; ?></td>
-                    <td><?php echo $values["item_quantity"]; ?></td>
-                    <td>$ <?php echo $values["item_price"]; ?></td>
-                    <td>$ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2); ?></td>
-                    <td><a href="Test_Cart/Cart.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span
-                                class="text-danger">Remove</span></a></td>
-                </tr>
-                <?php  
-                                    $total = $total + ($values["item_quantity"] * $values["item_price"]);  
-                               }  
-                          ?>
-                <tr>
-                    <td colspan="3" align="right">Total</td>
-                    <td align="right">$ <?php echo number_format($total, 2); ?></td>
-                    <td></td>
-                </tr>
-                <?php  
-                          }  
-                          ?>
+        
        
     </div>
     <br />
